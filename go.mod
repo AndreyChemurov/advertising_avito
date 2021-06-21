8966f65109acd2dabc747832efa2098129b2e794
AndreyChemurov <andrey.chemurov@gmail.com>
2021-06-21 22:15:10 +0300

    some tests removed

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module advertising_avito
 go 1.16
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.2
-	github.com/stretchr/testify v1.6.1
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -1,35 +1,8 @@
 package database
 
 import (
-	"context"
-	"regexp"
 	"testing"
-
-	"github.com/DATA-DOG/go-sqlmock"
-	_ "github.com/lib/pq" //
 )
-
-func TestDatabaseGetOne(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectBegin()
-	query := "SELECT name, link, price, description FROM advertisement INNER JOIN photos ON (advertisement.id=$1 and adv_id=$1);"
-	prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
-	prep.ExpectExec().WithArgs(1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
-	mock.ExpectCommit()
-
-	p := &postgres{
-		db: db,
-	}
-
-	if _, _, _, _, err = p.GetOne(context.Background(), 1, true); err != nil {
-		t.Errorf("error was not expected while get one: %v", err)
-	}
-}
 
 func TestDatabaseCreateFail(t *testing.T) {
 	_, err := GetDatabase("error-database")
